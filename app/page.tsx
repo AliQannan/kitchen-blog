635213a1f36401fdb82a679e0aff9395ee82bd3f
AliQannan <126164190+AliQannan@users.noreply.github.com>
2025-08-22 19:51:28 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function HomePage() {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
       <h1 className="text-4xl font-bold text-white">
         ali qannan basema
+        <a href="www.google.com"> 
+        
+        google
+        </a>
       </h1>
     </div>
   );
